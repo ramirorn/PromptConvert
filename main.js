961efce0de67273ac741df030fb486a009acbb89
Ramiro <ramiroroman306@gmail.com>
2025-05-11 11:17:45 -0300

    Se escribio el codigo que permite la conversión de ARS a USD

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
-console.log ()
+const historial = [];
+const tasaDeCambio = 1300;
+
+function convertCurrency(pesos) {
+    return pesos / tasaDeCambio;
+}
+
+function menu() {
+    let opcion;
+}
+
+
+
+
+do {
+    opcion = prompt("Seleccione una opción para operar: \n1) Divisa (ARS -> USD)\n2) Salir");
+    switch (opcion) {
+        case "1":
+            /*Conversion*/
+            let ingresoPesos = prompt("Ingrese el monto en pesos argentinos(ARS)");
+            if (ingresoPesos === null) break; /*Detecta si el usuario canceló un prompt*/
+            let pesos = parseFloat (ingresoPesos);
+            if (isNaN(pesos) || pesos <= 0) {
+                alert("Por favor ingrese un número válido mayor a 0.");
+                break;
+            } /*Verifica si el numero ingresado es un valor numerico y si es mayor a 0*/
+            let usd = convertCurrency(pesos);
+            alert(`Equivale a: ${usd.toFixed(2)} USD`); /*llama a la funcion covertCurrency para hacer el equivalente en USD y a ese resultado solo utiliza los primeros 2 decimales */
+
+    }
+
+} while (opcion !== "2")
